8d3f3699f9c8af5d2b780bc217fc94f58c99cdb4
ferre0160 <heydeferre@gmail.com>
2021-10-05 14:11:54 +0200

    first commit

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(
+            horizontal: 24,
+            vertical: 32,
+          ),
+          color: Color(0xFFF6F6F6),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 32),
+                child: Image(
+                  image: AssetImage('assets/logo.png'),
+                ),
+              ),
+              TaskCardWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TaskCardWidget extends StatelessWidget {
+  const TaskCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(20)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Get Started',
+              style: TextStyle(
+                  color: Color(0xFF211551),
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Text(
+                'Hello User! Welcome to the TODO app, this is a default task that you can edit or delete to start using the app.',
+                style: TextStyle(
+                    fontSize: 16, color: Color(0xFF868290), height: 1.5),
+              ),
+            )
+          ],
+        ));
+  }
+}
